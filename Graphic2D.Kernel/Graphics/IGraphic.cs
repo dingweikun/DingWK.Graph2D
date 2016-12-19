cc63426ad126d2b07d49728816ccfbfec8dbe1fd
Ding,Weikun <dingwk_bj@126.com>
2016-12-19 18:50:22 +0800

    Create IGraphic interface

---
+++ Graphic2D.Kernel/Graphics/IGraphic.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+
+namespace Graphic2D.Kernel.Graphics
+{
+    public interface IGraphic
+    {
+        IGraphic DefaultGraphic { get; } 
+
+        void Drawing(DrawingContext dc, Brush fill, Pen stroke);
+    }
+}
