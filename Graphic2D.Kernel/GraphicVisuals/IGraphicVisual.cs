ff88150f1f8fa6ae57b3f8fa8eaec0a496df8ff4
Ding,Weikun <dingwk_bj@126.com>
2016-12-20 15:45:53 +0800

    add IGraphicVisual

---
+++ Graphic2D.Kernel/GraphicVisuals/IGraphicVisual.cs
@@ -0,0 +1,25 @@
+﻿using Graphic2D.Kernel.Graphic;
+using System.ComponentModel;
+using System.Windows;
+
+namespace Graphic2D.Kernel.GraphicVisuals
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface IGraphicVisual : IGraphic, INotifyPropertyChanged
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        void UpdateTransform();
+        void UpdateVisualFill();
+        void UpdateVisualStroke();
+
+
+        void Rotate(double angle, Point center, bool IsLocalCenter);
+        void Move(double dx, double dy);
+        void Scale(double rx, double ry, double cx, double cy);
+
+    }
+}
