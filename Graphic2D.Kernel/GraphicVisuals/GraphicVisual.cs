58c00b8c2a49eb02996c1ed8b42d312da0d1b24f
Ding,Weikun <dingwk_bj@126.com>
2016-12-21 01:25:38 +0800

    add GraphicVisual class

---
+++ Graphic2D.Kernel/GraphicVisuals/GraphicVisual.cs
@@ -0,0 +1,181 @@
+﻿using Graphic2D.Kernel.Graphic;
+using Graphic2D.Kernel.Graphic.Geom;
+using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Media;
+
+namespace Graphic2D.Kernel.GraphicVisuals
+{
+    /// <summary>
+    /// 内部封装了图形对象的可视化元素，该元素提供了图形对象在 WPF 中的显示支持和变换操作。
+    /// The visual element which wraps the graphic object inside. 
+    /// The visual element provides rendering support in WPF and transform operations.
+    /// </summary>
+    public class GraphicVisual : DrawingVisual, IGraphicVisual
+    {
+        #region Internal field
+
+        // 要显示的内部图形对象。The inside graphic object to be rendering .
+        protected Graphic<IGeom> _graphic;
+
+        #endregion
+
+
+        #region Constructors
+
+        /// <summary>
+        /// 内部构造函数，只用于派生类 GraphicVisualGroup 初始化时使用。
+        /// A protected constructor which is only used by the derived class GraphicVisualGroup for initialization.
+        /// </summary>
+        protected GraphicVisual()
+        {
+            _graphic = new Graphic<IGeom>(new EmptyGeom());
+            UpdateTransform();
+        }
+
+        /// <summary>
+        /// 初始化一个显示对象实例。Initializes a GraphicVisual instance.
+        /// </summary>
+        public GraphicVisual(Graphic<IGeom> graphic)
+        {
+            if (graphic == null)
+                throw new ArgumentNullException();
+            _graphic = new Graphic<IGeom>(graphic);
+            UpdateTransform();
+            UpdateVisual();
+        }
+
+        /// <summary>
+        /// 初始化一个显示对象的副本实例。Initializes a new copy instance of specific graphic object.
+        /// </summary>
+        public GraphicVisual(GraphicVisual graphicVisual)
+            : this(graphicVisual._graphic)
+        {
+        }
+
+        #endregion
+
+
+        #region Method
+
+        /// <summary>
+        /// 更新可视化元素的显示。Updates visual for rendering. 
+        /// </summary>
+        public void UpdateVisual()
+        {
+            if (_graphic != null)
+            {
+                DrawingContext dc = RenderOpen();
+                _graphic.Geom.DrawGeom(dc, Fill, Stroke);
+                dc.Close();
+            }
+        }
+
+        #endregion
+
+
+        #region INotifyPropertyChanged interface implementation
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // 触发 PropertyChanged 事件的通用方法。
+        // A general method to raise the PropertyChanged event.
+        protected void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+
+        #region IGraphic interface members
+
+        public virtual double Angle
+        {
+            get { return _graphic.Angle; }
+            set
+            {
+                _graphic.Angle = value;
+                UpdateTransform();
+                NotifyPropertyChanged(nameof(Angle));
+            }
+        }
+
+        public virtual Brush Fill
+        {
+            get { return _graphic.Fill; }
+            set
+            {
+                _graphic.Fill = value;
+                UpdateVisual();
+                NotifyPropertyChanged(nameof(Fill));
+            }
+        }
+
+        public virtual Point Origin
+        {
+            get { return _graphic.Origin; }
+            set
+            {
+                _graphic.Origin = value;
+                UpdateTransform();
+                NotifyPropertyChanged(nameof(Origin));
+            }
+        }
+
+        public virtual Pen Stroke
+        {
+            get { return _graphic.Stroke; }
+            set
+            {
+                _graphic.Stroke = value;
+                UpdateVisual();
+                NotifyPropertyChanged(nameof(Stroke));
+            }
+        }
+
+        #endregion
+
+
+        #region IGraphicVisual interface members
+        
+        /// <summary>
+        /// 更新对象显示变换
+        /// </summary>
+        public virtual void UpdateTransform()
+        {
+            TransformGroup tr = new TransformGroup();
+            tr.Children.Add(new RotateTransform(Angle));
+            tr.Children.Add(new TranslateTransform(Origin.X, Origin.Y));
+            Transform = tr;
+        }
+
+        public virtual void UpdateVisualFill() => UpdateVisual();
+
+        public virtual void UpdateVisualStroke() => UpdateVisual();
+
+        public virtual void Rotate(double angle, Point center, bool IsLocalCenter = false)
+        {
+            Point cpot = IsLocalCenter ? Transform.Transform(center) : center;
+            Transform trans = new RotateTransform(angle, cpot.X, cpot.Y);
+            Origin = trans.Transform(Origin);
+            Angle += angle;
+            UpdateTransform();
+        }
+
+        public virtual void Move(double dx, double dy)
+        {
+            Origin += new Vector(dx, dy);
+            UpdateTransform();
+        }
+
+        public virtual void Scale(double rx, double ry, double cx, double cy)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+    }
+}
--- Graphic2D.Kernel/GraphicVisuals/IGraphicVisual.cs
+++ Graphic2D.Kernel/GraphicVisuals/IGraphicVisual.cs
@@ -4,22 +4,71 @@ using System.Windows;
 
 namespace Graphic2D.Kernel.GraphicVisuals
 {
-    /// <summary>
-    /// 
+    /// <summary> 
+    /// 提供有关显示更新和变换操作的方法。
+    /// Provides methods related to display updates and transform operations.
     /// </summary>
     public interface IGraphicVisual : IGraphic, INotifyPropertyChanged
     {
         /// <summary>
-        /// 
+        /// 更新几何变换状态。Update transformation state. 
         /// </summary>
         void UpdateTransform();
+
+        /// <summary>
+        /// 更新可视元素的填充状态。Update visual element fill state.
+        /// </summary>
         void UpdateVisualFill();
+
+        /// <summary>
+        /// 更新可视元素的轮廓绘制状态。Update visual element stroke drawing state.
+        /// </summary>
         void UpdateVisualStroke();
 
-
+        /// <summary>
+        /// 旋转操作。Rotation transform operation.
+        /// </summary>
+        /// <param name="angle">
+        /// 旋转角度，单位度。Rotation angle which is in degrees.
+        /// </param>
+        /// <param name="center">
+        /// 旋转中心点。Rotation transform center point.
+        /// </param>
+        /// <param name="IsLocalCenter">
+        /// 旋转中心点位置是否为局部坐标。Whether the rotation center point is in local coordinate.
+        /// </param>
         void Rotate(double angle, Point center, bool IsLocalCenter);
-        void Move(double dx, double dy);
-        void Scale(double rx, double ry, double cx, double cy);
+
+        /// <summary>
+        /// 比例缩放操作。Scale transform operation.
+        /// </summary>
+        /// <param name="scaleX">
+        /// X 轴的缩放比例。The x-axis scale transform factor.
+        /// </param>
+        /// <param name="scaleY">
+        /// Y 轴的缩放比例。The y-axis scale transform factor.
+        /// </param>
+        /// <param name="centerX">
+        /// 缩放中心点的 X 坐标。The x-coordinate of scale transform center point.
+        /// </param>
+        /// <param name="centerY">
+        /// 缩放中心点的 Y 坐标。The y-coordinate of scale transform center point.
+        /// </param>
+        /// <param name="IsLocalCenter">
+        /// 缩放中心点位置是否为局部坐标。Whether the scale center point is in local coordinate.
+        /// </param>
+        void Scale(double scaleX, double scaleY, double centerX, double centerY, bool IsLocalCenter);
+
+        /// <summary>
+        /// 平移操作。Translation transform operation.
+        /// </summary>
+        /// <param name="dx">
+        /// 沿 X 轴方向的移动距离。The distance to translate along the x-axis.
+        /// </param>
+        /// <param name="dy">
+        /// 沿 Y 轴方向的移动距离。The distance to translate along the y-axis.
+        /// </param>
+        void Translate(double dx, double dy);
 
     }
 }
